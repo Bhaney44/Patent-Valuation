c5a3dd8df0e2bd3755327063695bbc187b6217c3
B Seamus Haney <43055154+Bhaney44@users.noreply.github.com>
2020-06-01 14:03:55 -0400

    Add files via upload

--- Projections/3D_Patent_Model.py
+++ Projections/3D_Patent_Model.py
@@ -4,23 +4,35 @@
 import matplotlib.pyplot as plt
 from matplotlib import style
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import style
 
 #Other dependencies
 import numpy as np
 import csv
 
-def patent_file():
-    csvfile = open('qm_0.csv', 'r')
-    read = csv.reader(csvfile)
-        for row in read:
-            patent 
-
+style.use('ggplot')
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
+x = []
+y = []
+z = []
 
+with open('qm_2.csv','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        x.append(int(row[0]))
+        y.append(int(row[1]))
+        z.append(float(row[2]))
+
+
+plt.scatter(x,y,z, label='Patents')
+
+ax.set_xlabel('Claims')
+ax.set_ylabel('Cited Proior Art')
+ax.set_zlabel('Inventors')
+
+plt.title('Patents')
+plt.legend()
 plt.show()

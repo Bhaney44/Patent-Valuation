eed9c966fa5b0d66675bc405f10c57bf37ddfcf0
B Seamus Haney <43055154+Bhaney44@users.noreply.github.com>
2020-07-30 01:02:39 -0400

    Add files via upload

---
+++ Magic_Panda.py
@@ -0,0 +1,26 @@
+#Imports
+import pandas as pd
+from pandas import DataFrame
+
+#Define variable for file
+df = pd.read_csv('Trans_pre.csv')
+
+#Process Data
+print(df.head())
+print(df.tail())
+print(df.index)
+print(df.columns)
+print("-----")
+#Describe Data
+print(df.describe())
+#Median
+print('median')
+print(df.median())
+print("-----")
+#Mode
+print('mode')
+print(df.mode())
+print("-----")
+
+
+
